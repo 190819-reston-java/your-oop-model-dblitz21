3b0d21b5805f1c3e38341c2676b613e863a4a09f
dblitz <blitzy2k@gmail.com>
2019-08-27 16:03:44 -0400

    Started writing collection method

---
+++ src/main/java/com/revature/vehicles/VehicleCollectionDriver.java
@@ -0,0 +1,18 @@
+package com.revature.vehicles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class VehicleCollectionDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Car> carlist = new ArrayList<Car>();
+		carlist.add(new Car("Ferari", "red", 85));
+		carlist.add(new Car("Honda", "blue", 65));
+
+	}
+
+}
